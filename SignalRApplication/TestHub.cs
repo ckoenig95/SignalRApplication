206b405e33a5a1ea52059278e3d8d1701419c20c
Christian König <chk@uniserve.de>
2018-04-27 20:10:03 +0200

    Test

---
+++ SignalRApplication/TestHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRApplication
+{
+    public class TestHub : Hub
+    {
+        public byte[] GetData(int size) => new byte[size];
+    }
+}
